d14ed0340aca94a4b6d985c3758c0797a3f63cc6
NatalieChurch <nataliechurch12@gmail.com>
2025-04-10 20:35:59 -0500

    Added more colors and options to the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Routes, Route, Link } from "react-router-dom";
 import Red from './components/Red';
 import Blue from './components/Blue';
 import Home from './components/Home';
+import Green from './components/Green';
+import Orange from './components/Orange';
+import Purple from './components/Purple';
 
 
 function App() {
@@ -14,14 +17,20 @@ function App() {
       <div id="container">
 
         <div id="navbar">
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/">Home</Link>
+          <Link to="/blue" style={{color: "black"}}>Blue</Link>
+          <Link to="/red" style={{color:"black"}}>Red</Link>
+          <Link to="/green" style={{color:"black"}}>Green</Link>
+          <Link to="/purple" style={{color:"black"}}>Purple</Link>
+          <Link to="/orange" style={{color:"black"}}>Orange</Link>
+          <Link to="/" style={{color:"black"}}>Home</Link>
         </div>
         <div id="main-section">
           <Routes>
               <Route path="/blue" element={<Blue/>}></Route>
               <Route path="/red" element={<Red/>}></Route>
+              <Route path="/green" element={<Green/>}></Route>
+              <Route path="/purple" element={<Purple/>}></Route>
+              <Route path="/orange" element={<Orange/>}></Route>
               <Route path="/" element={<Home/>}></Route>
           </Routes>
         </div>
---
+++ src/components/Orange.jsx
@@ -0,0 +1,9 @@
+function Orange() {
+    return (
+        <div className="orange">
+            <h1>Orange</h1>
+        </div>
+    );
+}
+
+export default Orange
